f8cee9feb90234f0289fa72aa86d93295007001d
vaidehisinghal <90372180+vaidehisinghal@users.noreply.github.com>
2021-10-12 23:51:35 +0530

    volume slider added

---
+++ Assets/Scripts/volumeScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class volumeScript : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] AudioSource sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume" , 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+    public void ChangeVolume()
+    {
+        sound.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+    
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume" , volumeSlider.value);
+    }
+}
